57b8be9d25a50fe677af149c37bbd0a9f93def00
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2022-03-30 14:03:06 +0100

    function for encoding sentence

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,34 @@ function filterSentence(sentence) {
   return sentence;
 }
 
+function encodeSentence(sentence) {
+  sentence = filterSentence(sentence);
+  if (errorCheck(sentence)) {
+    return ""
+  } else {
+    let square = [];
+    let squareSides = Math.ceil(Math.sqrt(sentence.length));
+    for (let i = 0; i < squareSides; i++) {
+      square.push([]);
+    }
+    let sentenceArray = sentence.split("");
+    let index = 0;
+    sentenceArray.forEach(function (sentence) {
+      square[index].push(sentence);
+      index++;
+      if (index === squareSides) {
+        index = 0;
+      }
+    });
+    let results = ""
+    square.forEach(function (column) {
+      results += column.join("");
+    });
+    let splitResults = "";
+    for (let i = 0; i < results.length; i += 5) {
+      splitResults += results.slice(i, i + 5) + " ";
+    }
+    return splitResults;
+  }
+}
+
